8387cd69e33ae0ca6cec1bc501d9ee9b86f4b879
carlahauu <carlahau8@gmail.com>
2024-07-18 07:28:51 -0700

    Added NASA API

--- src/components/Exhibit.jsx
+++ src/components/Exhibit.jsx
@@ -2,15 +2,31 @@ import React from 'react'
 import Photo from './Photo'
 import PhotoName from './PhotoName'
 import SideBar from './SideBar'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function Exhibit() {
+
     const [showModal, setShowModal] = useState(false)
   
-
     function handleToggleModal(){
         setShowModal(!showModal)
     }
+
+    useEffect(() => {
+        async function fetchAPIData(){
+            const NASA_KEY = import.meta.env.VITE_NASA_API_KEY 
+            const url ='https://api.nasa.gov/planetary/apod' +
+            `?api_key=${NASA_KEY}`
+
+            try {
+                const res = await fetch(url)
+                const data = await res.json()
+                console.log('Data\n', data)
+            } catch (err) {
+                console.log(err.message)
+            }
+        }
+    }, [])
 
     return (
     <>
